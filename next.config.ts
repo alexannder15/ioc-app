92dfd3efcec82e8c23dd3f863211ad066d36d934
Alex Becerra <alexannder15@hotmail.com>
2025-08-19 20:41:46 -0500

    tryikng to show images

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from 'next';
 
-//const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  // Required for GitHub Pages (static HTML export)
-  // output: 'export',
-  // // GitHub Pages serves from a subpath: https://alexannder15.github.io/ioc-app/
-  // basePath: isProd ? '/ioc-app' : '',
-  // assetPrefix: isProd ? '/ioc-app/' : '',
-  // // Disable image optimization (since Vercel server isn’t available on GH Pages)
-  // images: {
-  //   unoptimized: true,
-  // },
+  //Required for GitHub Pages (static HTML export)
+  output: 'export',
+  // GitHub Pages serves from a subpath: https://alexannder15.github.io/ioc-app/
+  basePath: '/ioc-app',
+  assetPrefix: '/ioc-app/',
+  // Disable image optimization (since Vercel server isn’t available on GH Pages)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
