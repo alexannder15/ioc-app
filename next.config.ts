835c2e6cdc0c31c134e03837cfab86952b0c535f
Alex Becerra <alexannder15@hotmail.com>
2025-08-19 20:49:58 -0500

    trying to show images

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
   //Required for GitHub Pages (static HTML export)
   output: 'export',
   // GitHub Pages serves from a subpath: https://alexannder15.github.io/ioc-app/
-  basePath: '/ioc-app',
-  assetPrefix: '/ioc-app/',
+  basePath: process.env.PAGES_BASE_PATH,
   // Disable image optimization (since Vercel server isn’t available on GH Pages)
   images: {
     unoptimized: true,
