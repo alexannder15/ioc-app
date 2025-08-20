83f1f7d7c5320c92726226751b09ef3da8d6677b
Alex Becerra <alexannder15@hotmail.com>
2025-08-19 20:38:08 -0500

    print environment

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
+//const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   // Required for GitHub Pages (static HTML export)
-  output: 'export',
-
-  // GitHub Pages serves from a subpath: https://alexannder15.github.io/ioc-app/
-  basePath: isProd ? '/ioc-app' : '',
-  assetPrefix: isProd ? '/ioc-app/' : '',
-
-  // Disable image optimization (since Vercel server isn’t available on GH Pages)
-  images: {
-    unoptimized: true,
-  },
+  // output: 'export',
+  // // GitHub Pages serves from a subpath: https://alexannder15.github.io/ioc-app/
+  // basePath: isProd ? '/ioc-app' : '',
+  // assetPrefix: isProd ? '/ioc-app/' : '',
+  // // Disable image optimization (since Vercel server isn’t available on GH Pages)
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
